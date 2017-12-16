b7c4d8f454746af4376284fe966157ffabf0c595
Frank Thelen <frank.thelen@telefonica.com>
2017-12-16 12:00:33 +0100

    Hapi v17 support

--- index.js
+++ index.js
@@ -1,30 +1,39 @@
-module.exports = addCorsHeaders
+const pkg = require('./package.json');
 
-function addCorsHeaders (request, reply) {
-  if (!request.headers.origin) {
-    return reply.continue()
-  }
+const register = (server, {
+  maxAge = 60 * 10, // 10 minutes
+}) => {
+  server.ext({
+    type: 'onPreResponse',
+    method: (request, h) => {
+      if (!request.headers.origin) {
+        return h.continue;
+      }
 
-  // depending on whether we have a boom or not,
-  // headers need to be set differently.
-  var response = request.response.isBoom ? request.response.output : request.response
+      // depending on whether we have a boom or not,
+      // headers need to be set differently.
+      const response = request.response.isBoom ? request.response.output : request.response;
 
-  response.headers['access-control-allow-origin'] = request.headers.origin
-  response.headers['access-control-allow-credentials'] = 'true'
-  if (request.method !== 'options') {
-    return reply.continue()
-  }
+      response.headers['access-control-allow-origin'] = request.headers.origin;
+      response.headers['access-control-allow-credentials'] = 'true';
+      if (request.method !== 'options') {
+        return h.continue;
+      }
 
-  response.statusCode = 200
-  response.headers['access-control-expose-headers'] = 'content-type, content-length, etag'
-  response.headers['access-control-max-age'] = 60 * 10 // 10 minutes
-  // dynamically set allowed headers & method
-  if (request.headers['access-control-request-headers']) {
-    response.headers['access-control-allow-headers'] = request.headers['access-control-request-headers']
-  }
-  if (request.headers['access-control-request-method']) {
-    response.headers['access-control-allow-methods'] = request.headers['access-control-request-method']
-  }
+      response.statusCode = 200;
+      response.headers['access-control-expose-headers'] = 'content-type, content-length, etag';
+      response.headers['access-control-max-age'] = maxAge;
+      // dynamically set allowed headers & method
+      if (request.headers['access-control-request-headers']) {
+        response.headers['access-control-allow-headers'] = request.headers['access-control-request-headers'];
+      }
+      if (request.headers['access-control-request-method']) {
+        response.headers['access-control-allow-methods'] = request.headers['access-control-request-method'];
+      }
 
-  reply.continue()
-}
+      return h.continue;
+    },
+  });
+};
+
+module.exports = { register, pkg };
